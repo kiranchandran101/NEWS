0d72b36144b3a1ea5894b107a90281de9c19b4df
Kiranchandran <167624231+kiranchandran101@users.noreply.github.com>
2024-04-20 18:15:56 +0000

    loggedin_page

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def login():
   return render_template('login.html')
 
+@app.route('/loggedin')
+def logged_successfull():
+  return render_template('loggedin.html')
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
